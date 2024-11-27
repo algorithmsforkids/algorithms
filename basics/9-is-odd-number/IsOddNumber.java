b36c02847f51dce2c90ef00222018446388a967a
Bunlong <bunlong.van@gmail.com>
2024-11-27 16:46:22 +0700

    Refactor

---
+++ basics/9-is-odd-number/IsOddNumber.java
@@ -0,0 +1,11 @@
+public class IsOddNumber {
+  public static void main(String[] args) {
+    System.out.println(isOddNumber(5));
+  }
+
+  public static boolean isOddNumber(int n) {
+    return n % 2 != 0;
+  }
+}
+
+// java IsOddNumber.java
