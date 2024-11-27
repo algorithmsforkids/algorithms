ac547a75ea7034c6abc8f0d7f9c1db2e853d5fa0
Bunlong <bunlong.van@gmail.com>
2024-11-27 16:45:58 +0700

    Refactor

--- basics/8-is-even-number/IsEvenNumber.java
+++ basics/8-is-even-number/IsEvenNumber.java
@@ -1,15 +1,8 @@
 public class IsEvenNumber {
   public static void main(String[] args) {
-    System.out.println(isEvenNumber(5));
+    System.out.println(isEvenNumber(4));
   }
 
-  // public static boolean isEvenNumber(int n) {
-  //   boolean isEven = false;
-  //   if (n % 2 == 0) {
-  //     isEven = true;
-  //   }
-  //   return isEven;
-  // }
   public static boolean isEvenNumber(int n) {
     return n % 2 == 0;
   }
