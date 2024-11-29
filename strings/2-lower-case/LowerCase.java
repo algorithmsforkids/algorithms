a83e39b5ed5f894dca92d90ab2604bb1258e6a9d
Bunlong <bunlong.van@gmail.com>
2024-11-29 09:38:43 +0700

    LowerCase

---
+++ strings/2-lower-case/LowerCase.java
@@ -0,0 +1,22 @@
+public class LowerCase {
+  public static void main(String[] args) {
+    System.out.println(toLowerCase("ABC").equals("ABC".toLowerCase()));
+    System.out.println(toLowerCase("ABC123").equals("ABC123".toLowerCase()));
+    System.out.println(toLowerCase("abcABC").equals("abcABC".toLowerCase()));
+    System.out.println(toLowerCase("abc123ABC").equals("abc123ABC".toLowerCase()));
+  }
+
+  public static String toLowerCase(String str) {
+    char[] chars = str.toCharArray();
+
+    for (int i = 0; i < chars.length; ++i) {
+      if (Character.isLetter(chars[i]) && Character.isUpperCase(chars[i])) {
+        chars[i] = Character.toLowerCase(chars[i]);
+      }
+    }
+
+    return new String(chars);
+  }
+}
+
+// java LowerCase.java
